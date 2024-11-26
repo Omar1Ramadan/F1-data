7b570ed28d370e3034d1940d04b322de583fcfa1
Hamza Elkababji <hamza.kababji@gmail.com>
2024-11-25 23:24:45 -0500

    fixed up file structure by creating a seperate db.js file for database connection. Also made a function to get rid of repetetive operations in server.js called handleCrudOperations. Also made a function to build your own sql quesry-coudl be used for the frontend.

--- SRC/server/server.js
+++ SRC/server/server.js
@@ -1,147 +1,91 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mysql = require('mysql2');
 const path = require('path');
-require('dotenv').config();
-
 const app = express();
-const PORT = 5000;
+app.use(express.json());
 
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
+const db = require('./db'); // Ensure this path is correct
 
-// Database Connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to MySQL Database!');
-});
+// Helper function to build query
+function buildQuery(table, params) {
+  // Implement your query building logic here
+  return { query: `SELECT * FROM ${table}`, values: [] };
+}
 
-// API Endpoints for Regular Users (GET Routes)
-app.get('/driver', (req, res) => {
-  db.query('SELECT * FROM Driver', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving drivers');
+// Function to handle CRUD operations
+function handleCrudOperations(table) {
+  return (req, res) => {
+    switch (req.method) {
+      case 'GET':
+        const { query, values } = buildQuery(table, req.query);
+        db.query(query, values, (err, results) => {
+          if (err) {
+            return res.status(500).send(`Error retrieving ${table.toLowerCase()}s`);
+          }
+          res.json(results);
+        });
+        break;
+      case 'POST':
+        const postColumns = Object.keys(req.body).join(', ');
+        const postPlaceholders = Object.keys(req.body).map(() => '?').join(', ');
+        const postValues = Object.values(req.body);
+        const postQuery = `INSERT INTO ${table} (${postColumns}) VALUES (${postPlaceholders})`;
+        db.query(postQuery, postValues, (err, results) => {
+          if (err) {
+            return res.status(500).send(`Error adding ${table.toLowerCase()}`);
+          }
+          res.status(201).send(`${table} added successfully`);
+        });
+        break;
+      case 'PUT':
+        const putId = req.body[`${table}_ID`];
+        const putUpdates = Object.keys(req.body).filter(key => key !== `${table}_ID`).map(key => `${key} = ?`).join(', ');
+        const putValues = [...Object.values(req.body).filter((_, index) => Object.keys(req.body)[index] !== `${table}_ID`), putId];
+        const putQuery = `UPDATE ${table} SET ${putUpdates} WHERE ${table}_ID = ?`;
+        db.query(putQuery, putValues, (err, results) => {
+          if (err) {
+            return res.status(500).send(`Error updating ${table.toLowerCase()}`);
+          }
+          res.send(`${table} updated successfully`);
+        });
+        break;
+      case 'DELETE':
+        const deleteId = req.body[`${table}_ID`];
+        const deleteQuery = `DELETE FROM ${table} WHERE ${table}_ID = ?`;
+        db.query(deleteQuery, [deleteId], (err, results) => {
+          if (err) {
+            return res.status(500).send(`Error deleting ${table.toLowerCase()}`);
+          }
+          res.send(`${table} deleted successfully`);
+        });
+        break;
+      default:
+        res.status(405).send('Method Not Allowed');
     }
-    res.json(results); // Send the list of drivers to the client
-  });
-});
+  };
+}
 
-app.get('/constructor', (req, res) => {
-  db.query('SELECT * FROM Constructor', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving constructors');
-    }
-    res.json(results); // Send the list of constructors to the client
-  });
-});
+// Define routes for each table
+const tables = ['Circuit', 'Driver', 'Constructor', 'Season', 'GrandPrix', 'MainRace', 'QualificationRace', 'QualifyingResult', 'RaceResult', 'PitStop', 'DriverEntry'];
 
-app.get('/circuit', (req, res) => {
-  db.query('SELECT * FROM Circuit', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving circuits');
-    }
-    res.json(results); // Send the list of circuits to the client
-  });
-});
-
-app.get('/season', (req, res) => {
-  db.query('SELECT * FROM Season', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving seasons');
-    }
-    res.json(results); // Send the list of seasons to the client
-  });
-});
-
-app.get('/grandprix', (req, res) => {
-  db.query('SELECT * FROM GrandPrix', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving grand prix');
-    }
-    res.json(results); // Send the list of grand prix to the client
-  });
-});
-
-app.get('/qualificationrace', (req, res) => {
-  db.query('SELECT * FROM QualificationRace', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving qualification races');
-    }
-    res.json(results); // Send the list of qualification races to the client
-  });
-});
-
-app.get('/qualifyingresult', (req, res) => {
-  db.query('SELECT * FROM QualifyingResult', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving qualifying results');
-    }
-    res.json(results); // Send the list of qualifying results to the client
-  });
-});
-
-app.get('/driverentry', (req, res) => {
-  db.query('SELECT * FROM DriverEntry', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving driver entries');
-    }
-    res.json(results); // Send the list of driver entries to the client
-  });
-});
-
-app.get('/mainrace', (req, res) => {
-  db.query('SELECT * FROM MainRace', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving main races');
-    }
-    res.json(results); // Send the list of main races to the client
-  });
-});
-
-app.get('/pitstop', (req, res) => {
-  db.query('SELECT * FROM PitStop', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving pit stops');
-    }
-    res.json(results); // Send the list of pit stops to the client
-  });
-});
-
-app.get('/raceresult', (req, res) => {
-  db.query('SELECT * FROM RaceResult', (err, results) => {
-    if (err) {
-      return res.status(500).send('Error retrieving race results');
-    }
-    res.json(results); // Send the list of race results to the client
-  });
+tables.forEach(table => {
+  app.route(`/${table.toLowerCase()}`)
+    .all(handleCrudOperations(table));
 });
 
 app.get('/api', (req, res) => {
   res.send('Hello from the server!');
 });
 
-// Serve React Static Files (frontend)
-app.use(express.static(path.join(__dirname, '../client/build')));
-
-// Catch-All Route to Serve React App for Non-API Requests
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-// Conditionally start the server only if NOT in the test environment
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
 
-module.exports = app;  // Export the app for testing
+module.exports = app; // Ensure this line is added to export the app for testing
